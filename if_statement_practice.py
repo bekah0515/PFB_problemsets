afaff2034f17a621993306f7090b849865c065a4
pfb2024 <pfb2024@info17.cshl.edu>
2024-10-15 16:47:34 -0400

    practiced nested tests

--- if_statement_practice.py
+++ if_statement_practice.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 #assign value to my variable
-color = 'yellow'
+mynumber = 605
 
-if 'yell' in color:
-  print('of course')
+#Test if my number is positive or negative
+if mynumber < 0:
+  print(mynumber, 'is negative')
+elif mynumber > 0:
+  print(mynumber, 'is positive')
+  if mynumber < 50:
+    print(mynumber,'is smaller than 50')
+  if mynumber > 50:
+    if mynumber%3 == 0:
+      print(mynumber, 'is greater than 50 and divisible by 3')
+  if mynumber%2 == 0:
+    print(mynumber, 'is even')
+  if mynumber%2 != 0:
+    print(mynumber, 'is odd')
 else:
-  print('Not true')
+  print('my number is 0')
 
