7a3be423875f2b7fe70b06ef4ba21af27bf39a5e
pfb2024 <pfb2024@info17.cshl.edu>
2024-10-17 11:51:57 -0400

    moved files around

--- dictionaries.py
+++ dictionaries.py
@@ -12,6 +12,6 @@ my_favs_dict['organism'] = 'dog'
 fav_thing = sys.argv[1]
 new_fav_thing = sys.argv[2]
 my_favs_dict[fav_thing] = new_fav_thing
-print(f'My new favorite {fav_thing} is {my_favs_dict[fav_thing]}')
+print(f'My favorite {fav_thing} is {my_favs_dict[fav_thing]}')
 print(my_favs_dict)
 
